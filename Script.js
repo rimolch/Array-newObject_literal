942910b0fa94d40b951fdd982475d61419479622
Rimol chakma <67545874+rimolch@users.noreply.github.com>
2020-09-24 09:02:39 +0600

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,12 @@
+var array = new Array();
+array[0] = " Yaakov ";
+array[1] = " John " ;
+array[2] = function(name){
+console.log( " Hello " + name);
+}
+array[2](array[0]);
+array[2](array[1]);
+array[3] = { Course: " HTML , CSS & JS "
+Topic: "JavaScript array new object literal"};
+console.log(array[3].Course);
+console.log(array[3].Topic);
